8b6e8eb9e76e4cec178731cb2d6cf77fafe9c211
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-12 10:15:31 -0500

    added round counter and part of comparison

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,75 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  //event listeners
+  $('#playerInput').on('submit', addGuess);
+  
+
 }
+
+function getGuess() {
+  //getting guesses from the server with ajax
+  $.ajax({
+    method: 'GET',
+    URL: '/guesses'
+  }).then(function(response){  // this function will hold the data we get from the server
+    console.log('success', response);
+    renderToDom(response)
+  }).catch(function(error){ //this will catch any error and show it at the dom
+    alert('request failed!'); 
+    console.log('request failed:', error)
+  })
+
+  //bellow function will make sure we get our previous guesses listed
+  function renderToDom(guesses){
+    $('#guesses').empty(); //we take guesses list and empty it so we can input new ones
+    for(guess of guesses) {//we loop through the guess list and spit out the player name and number
+      $('#guesses').append(`
+      <li>guess number: ${playerGuesses.guess} player name: ${playerGuesses.player}</li>
+      `)
+
+    }
+
+  }
+ let roundCount = 0;
+  function addGuess(event){
+    preventDefault(event) //this will prevent browser to refresh and it will hold our values
+    //defining our inputs
+    const playerOneNumber = ('#numOne').val(); 
+    const playerTwoNumber = ('#numTwo').val();
+
+    //clear input
+    ('#numOne').val('');
+    ('#numTwo').val('');
+
+    //counter starts when submit button is pressed
+
+    roundCount++;
+    $('#roundCounter').text(roundCount)
+
+    $.ajax({ //ajax is giving (posting) data to server
+      method: 'POST',
+      url: '/guesses',
+      data:{
+        playerOne: playerOneNumber,
+        playerTwo: playerTwoNumber
+      }
+    
+    }).then(function(response){ //when in post we dont need to worry about response
+        console.log('yay it works')
+        getGuess()
+
+    }).catch(function(error){
+      alert('error with guesses post');
+      console.log('error with post', error);
+  
+  }) 
+
+
+
+  }
+  
+  addGuess()
+}
+
--- server/server.js
+++ server/server.js
@@ -3,13 +3,66 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+
+let playerGuesses = [
+  {player:'Ismail', Guess: '1'},
+  {player:'Sabina', Guess: '2'}
+]
+
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+function getRandomNum (min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 // GET & POST Routes go here
+//get
+app.get('/guesses', function(req,res){
+  console.log('request for the guesses was made');
+  res.send(playerGuesses) //this will give us player guesses
+})
+//post
+app.post('/guesses', function(req,res){
+  console.log('post my guesses', req.body);
+  playerGuesses.push(req.body); //so we can push new guess 
+  res.sendStatus(201) //server response
+  
+})
+
+
+//Random number function
+function randomNumComparison(){
+  let randomNumber = getRandomNum()
+  if( randomNumber === playerGuesses.Guess){
+    return ''
+
+  }
+  if(randomNumber < playerGuesses.Guess){
+    return ''
+  }
+  if(randomNumber > playerGuesses.Guess){
+    return ''
+  }
+}
+
+//get number 
+// app.get('/randomNumber', function(req,res){
+//   console.log('request for the random numbers were made');
+//   res.send() //this will give us numbers from random number generator
+// })
+// //post number
+// app.post('/randomNumber', function(req,res){
+//   console.log('post my random numbers', req.body);
+//   playerGuesses.push(req.body); //so we can push new numbers from our function
+//   res.sendStatus(201) //server response
+// })
+
+
 
 
 app.listen(PORT, () => {
